d59e4de819967f408f6517a7f231895ced3a5c95
Sankalp Arora <sankalpnitj@gmail.com>
2020-04-03 09:27:59 +0530

    Handled some errors

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -7,6 +7,8 @@ const geocode = (address, callback) => {
     request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to location services.', undefined);
+        } else if(body.features === undefined) {
+            callback('Please type in the location.', undefined);
         } else if (body.features.length === 0) {
             callback('Unable to find location. Try another search.', undefined);
         } else {
